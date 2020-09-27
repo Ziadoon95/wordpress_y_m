960b63fa30def79603f440213c7ab1ccd9298b39
zaidun <zaidun10@yahoo.com>
2020-09-27 21:48:26 +0200

    yes

--- wordpress/wp-content/themes/zYm/index.php
+++ wordpress/wp-content/themes/zYm/index.php
@@ -1,9 +1,43 @@
 <?php get_header();?>
 <?php
-while (have_posts()) :
+/* while (have_posts()) :
 the_post();
 the_title();
 the_content(); endwhile;
+
+ */
+
+?>
+
+<?php if ( have_posts() ) : ?>
+<?php
+// Start the loop.
+while ( have_posts() ) : the_post();
+/*
+* Include the Post-Format-specific template for
+the content.
+* If you want to override this in a child
+theme, then include a file
+* called content-___.php (where ___ is the Post
+Format name) and that will be used instead.
+*/
+get_template_part( 'template/content',
+get_post_format() );
+// End the loop.
+endwhile;
+// Previous/next page navigation.
+/* the_posts_pagination( array(
+'prev_text' => __( 'Previous page',
+'spritz' ),
+'next_text' => __( 'Next page',
+'spritz' ),
+'before_page_number' => '<span class="meta-nav
+screen-reader-text">' . __( 'Page', 'spritz' ) . ' </span>',
+) ); */
+// If no content, include the "No posts found" template.
+else :
+get_template_part( 'template/content', 'none' );
+endif;
 ?>
   <body class="header-collapse">
  
